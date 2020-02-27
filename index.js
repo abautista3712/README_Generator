318ff917d4e0db77b4eb835172f5630c8ec49a8f
Abelard Bautista <abelardbautista@gmail.com>
2020-02-27 14:48:14 -0800

    Added require(fs) and wrote code to append hard coded text.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Test inside Node");
+var fs = require("fs");
+
+fs.appendFile("README.md", "\r\nTesting Part IV", function(err) {
+  if (err) {
+    console.log("Error");
+  } else {
+    console.log("Write File Success!");
+  }
+});
