f611eb694c8b2feef219f27442a76c1712ce0d35
Abelard Bautista <abelardbautista@gmail.com>
2020-02-27 15:16:13 -0800

    index.js prompts question and logs user response in console.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var fs = require("fs");
+var inquirer = require("inquirer");
 
-fs.appendFile("README.md", "\r\nTesting Part IV", function(err) {
-  if (err) {
-    console.log("Error");
-  } else {
-    console.log("Write File Success!");
-  }
-});
+inquirer
+  .prompt([
+    {
+      type: "Input",
+      name: "TestValue",
+      message: "Testing if inquirer works Q1"
+    }
+  ])
+  .then(function(response) {
+    console.log(response);
+  });
+
+// fs.appendFile("README.md", "\r\nTesting Part IV", function(err) {
+//   if (err) {
+//     console.log("Error");
+//   } else {
+//     console.log("Write File Success!");
+//   }
+// });
