e4daf37a44d65e3080e50543c0e95bfe91195c14
Abelard Bautista <abelardbautista@gmail.com>
2020-03-06 23:57:54 -0800

    Added formatting up to and including table of contents section.

--- index.js
+++ index.js
@@ -48,27 +48,54 @@ inquirer
     }
   ])
   .then(function(response) {
-    fs.appendFile("README.md", response.gitHubUsername, function(err) {
+    fs.appendFile("README.md", "\r\n" + response.gitHubUsername, function(err) {
       if (err) {
         console.log("Error");
       } else {
         console.log("Write File 1 Success!");
       }
     });
-    fs.appendFile("README.md", "\r\n" + response.projectName, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 2 Success!");
+    fs.appendFile(
+      "README.md",
+      "\r\n# " +
+        response.projectName +
+        "\r\nBadge Goes Here | Link to GitHub Goes Here\r\n",
+      function(err) {
+        if (err) {
+          console.log("Error");
+        } else {
+          console.log("Write File 2 Success!");
+        }
       }
-    });
-    fs.appendFile("README.md", "\r\n" + response.description, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 3 Success!");
+    );
+    fs.appendFile(
+      "README.md",
+      "\r\n## Description\r\n\r\n" + response.description + "\r\n",
+      function(err) {
+        if (err) {
+          console.log("Error");
+        } else {
+          console.log("Write File 3 Success!");
+        }
       }
-    });
+    );
+    fs.appendFile(
+      "README.md",
+      "\r\n## Table of Contents\r\n" +
+        "\r\n* [Installation](#installation)\r\n" +
+        "\r\n* [Usage](#usage)\r\n" +
+        "\r\n* [License](#license)\r\n" +
+        "\r\n* [Contributing](#contributing)\r\n" +
+        "\r\n* [Test](#tests)\r\n" +
+        "\r\n* [Questions](#questions)\r\n",
+      function(err) {
+        if (err) {
+          console.log("Error");
+        } else {
+          console.log("Write Table of Contents Success!");
+        }
+      }
+    );
     fs.appendFile("README.md", "\r\n" + response.license, function(err) {
       if (err) {
         console.log("Error");
@@ -76,34 +103,36 @@ inquirer
         console.log("Write File 4 Success!");
       }
     });
-    fs.appendFile("README.md", "\r\n" + response.installDep, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 5 Success!");
-      }
-    });
-    fs.appendFile("README.md", "\r\n" + response.runTest, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 6 Success!");
-      }
-    });
-    fs.appendFile("README.md", "\r\n" + response.usingRepo, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 7 Success!");
-      }
-    });
-    fs.appendFile("README.md", "\r\n" + response.contributeRepo, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 8 Success!");
-      }
-    });
+    // fs.appendFile("README.md", "5\r\n" + response.installDep, function(err) {
+    //   if (err) {
+    //     console.log("Error");
+    //   } else {
+    //     console.log("Write File 5 Success!");
+    //   }
+    // });
+    // fs.appendFile("README.md", "6\r\n" + response.runTest, function(err) {
+    //   if (err) {
+    //     console.log("Error");
+    //   } else {
+    //     console.log("Write File 6 Success!");
+    //   }
+    // });
+    // fs.appendFile("README.md", "7\r\n" + response.usingRepo, function(err) {
+    //   if (err) {
+    //     console.log("Error");
+    //   } else {
+    //     console.log("Write File 7 Success!");
+    //   }
+    // });
+    // fs.appendFile("README.md", "8\r\n" + response.contributeRepo, function(
+    //   err
+    // ) {
+    //   if (err) {
+    //     console.log("Error");
+    //   } else {
+    //     console.log("Write File 8 Success!");
+    //   }
+    // });
   });
 
 // $.ajax({
