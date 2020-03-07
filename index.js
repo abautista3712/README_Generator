c4d4d836fcf99997e5a9b9d6f2e553494c84e7cb
Abelard Bautista <abelardbautista@gmail.com>
2020-03-07 00:37:30 -0800

    Refactored code to include writeStream to order sections in README.md.

--- index.js
+++ index.js
@@ -19,12 +19,6 @@ inquirer
       message: "Please provide a brief description of your project:"
     },
     {
-      type: "list",
-      name: "license",
-      message: "Please select a license for your project:",
-      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-    },
-    {
       type: "Input",
       name: "installDep",
       message: "What command should be run to install dependencies?",
@@ -32,120 +26,62 @@ inquirer
     },
     {
       type: "Input",
-      name: "runTest",
-      message: "What command should be run to run tests?",
-      default: "npm test"
+      name: "usingRepo",
+      message: "What does the user need to know about usage of the repo?"
     },
     {
-      type: "Input",
-      name: "usingRepo",
-      message: "What does the user need to know about using the repo?"
+      type: "list",
+      name: "license",
+      message: "Please select a license for your project:",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
     },
     {
       type: "Input",
       name: "contributeRepo",
       message: "What does the user need to know about contributing to the repo?"
+    },
+    {
+      type: "Input",
+      name: "runTest",
+      message: "What command should be run to run tests?",
+      default: "npm test"
     }
   ])
   .then(function(response) {
-    fs.appendFile("README.md", "\r\n" + response.gitHubUsername, function(err) {
-      if (err) {
-        console.log("Error");
-      } else {
-        console.log("Write File 1 Success!");
-      }
-    });
-    fs.appendFile(
-      "README.md",
+    const log = fs.createWriteStream("README.md");
+    log.write("\r\n" + response.gitHubUsername);
+    log.write(
       "\r\n# " +
         response.projectName +
-        "\r\nBadge Goes Here | Link to GitHub Goes Here\r\n",
-      function(err) {
-        if (err) {
-          console.log("Error");
-        } else {
-          console.log("Write File 2 Success!");
-        }
-      }
+        "\r\nBadge Goes Here | Link to GitHub Goes Here\r\n"
     );
-    fs.appendFile(
-      "README.md",
-      "\r\n## Description\r\n\r\n" + response.description + "\r\n",
-      function(err) {
-        if (err) {
-          console.log("Error");
-        } else {
-          console.log("Write File 3 Success!");
-        }
-      }
-    );
-    fs.appendFile(
-      "README.md",
+    log.write("\r\n## Description\r\n\r\n" + response.description + "\r\n");
+    log.write(
       "\r\n## Table of Contents\r\n" +
         "\r\n* [Installation](#installation)\r\n" +
         "\r\n* [Usage](#usage)\r\n" +
         "\r\n* [License](#license)\r\n" +
         "\r\n* [Contributing](#contributing)\r\n" +
         "\r\n* [Test](#tests)\r\n" +
-        "\r\n* [Questions](#questions)\r\n",
-      function(err) {
-        if (err) {
-          console.log("Error");
-        } else {
-          console.log("Write Table of Contents Success!");
-        }
-      }
+        "\r\n* [Questions](#questions)\r\n"
     );
-    // fs.appendFile("README.md", "\r\n" + response.license, function(err) {
-    //   if (err) {
-    //     console.log("Error");
-    //   } else {
-    //     console.log("Write File 4 Success!");
-    //   }
-    // });
-    fs.appendFile(
-      "README.md",
+    log.write(
       "\r\n## Installation\r\n" +
         "\r\nTo install necessary dependencies, run the following command:\r\n" +
         "\r\n```\r\n" +
         response.installDep +
-        "\r\n```\r\n",
-      function(err) {
-        if (err) {
-          console.log("Error");
-        } else {
-          console.log("Write File 4 Success!");
-        }
-      }
+        "\r\n```\r\n"
     );
-    // fs.appendFile("README.md", "6\r\n" + response.runTest, function(err) {
-    //   if (err) {
-    //     console.log("Error");
-    //   } else {
-    //     console.log("Write File 6 Success!");
-    //   }
-    // });
-    // fs.appendFile("README.md", "7\r\n" + response.usingRepo, function(err) {
-    //   if (err) {
-    //     console.log("Error");
-    //   } else {
-    //     console.log("Write File 7 Success!");
-    //   }
-    // });
-    // fs.appendFile("README.md", "8\r\n" + response.contributeRepo, function(
-    //   err
-    // ) {
-    //   if (err) {
-    //     console.log("Error");
-    //   } else {
-    //     console.log("Write File 8 Success!");
-    //   }
-    // });
+    log.write("\r\n## Usage\r\n" + "\r\n" + response.usingRepo + "\r\n");
+    log.write("\r\n## License\r\n" + "\r\n" + response.license + "\r\n");
+    log.write(
+      "\r\n## Contributing\r\n" + "\r\n" + response.contributeRepo + "\r\n"
+    );
+    log.write(
+      "\r\n## Installation\r\n" +
+        "\r\nTo run tests, run the following command:\r\n" +
+        "\r\n```\r\n" +
+        response.runTest +
+        "\r\n```\r\n"
+    );
   });
-
-// $.ajax({
-//   url: "https://github.com/login/oauth/authorize"
-//   method: "GET"
-// }).then(function(response){
-//   console.log("ajax test success")
-// })
